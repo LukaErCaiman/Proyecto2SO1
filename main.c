cdbc7395e81c83db8328da15469dab6cb5e32c02
Luis Silvera <Luis_D_Luka@hotmail.com>
2021-12-12 16:58:29 -0400

    main, funcion declarada, una variable + funcion

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void Opciones(int argc, char* argv[]);
+
+int main(int argc, char* argv[])
+{
+    int carnet = atoi(argv[1]);
+
+    return 0;
+}
+
+void Opciones(int argc, char* argv[])
+{
+
+}
