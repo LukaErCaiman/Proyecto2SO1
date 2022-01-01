274e1c97218c3db198f7801943e4a95845f155c8
Luis Silvera <Luis_D_Luka@hotmail.com>
2022-01-01 15:34:48 -0400

    Simplificacion para colaboracion

--- RecorreArchivos.c
+++ RecorreArchivos.c
@@ -3,25 +3,4 @@
 #include <string.h>
 #include <dirent.h>
 
-int RecorreArchivos(char* nombreDirectorio){
-    DIR* dir = opendir(strcat("./", nombreDirectorio) );
-
-    if (dir == NULL){
-        return 1;
-    }
-
-    struct dirent* file;
-    file = readdir(dir);
-    while(file != NULL){
-        char* found = strstr(file->d_name, nombreDirectorio);
-        
-        if (found != NULL){
-            //Lee archivo
-            break;
-        }
-    }
-
-    closedir(dir);
-
-    return 0;
-}
+/* Funcion para recorrer archivos, dado un directorio*/
